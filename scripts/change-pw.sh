683604f42683afff2ca50ad2486ebfd94bfd0095
fcarrion001 <fpc1118@gmail.com>
2017-07-20 16:24:41 -0400

    Ran curl requests for user table
    Switched the url in assets/scripts/config.js to the proper location
    Added change-pw.sh to test update action
    Added sign-in.sh and sign-up.sh to test create action
    Added sign-out.sh to test destroy action
    All actions tested have succeeded.

---
+++ scripts/change-pw.sh
@@ -0,0 +1,13 @@
+curl "https://franklin-budget-calculator.herokuapp.com/change-password/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Authorization: Token token=${TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "passwords": {
+      "old": "'"${OLDPW}"'",
+      "new": "'"${NEWPW}"'"
+    }
+  }'
+
+echo
