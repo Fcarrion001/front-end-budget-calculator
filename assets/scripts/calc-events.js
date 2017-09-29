502e37715597c52fc0c129277e6cf063f40a0e89
fcarrion001 <fpc1118@gmail.com>
2017-09-29 11:03:36 -0400

    Update works with table, added styling
    Upon selecting a row the update form is automatically populated with
    selected data. The user can then alter the data and click the update
    button to update.
    Added some images to the images directory.
    Used one of the images for the body background.
    Adjusted styling for increased visibility.
    Improved the introduction paragraph.
    Moved the update form html out of handlebars and into index.html.

--- assets/scripts/calc-events.js
+++ assets/scripts/calc-events.js
@@ -24,12 +24,18 @@ const rowSelector = function () {
     $('tr.selected').removeClass('selected')
     $(this).addClass('selected')
   }
+  const name = $('.selected').children().html()
+  // get cashflow value of selected row
+  const value = $('.selected td:eq(1)').text()
+  $('.cashflow-name').val(name)
+  $('.cashflow-value').val(value)
 }
-
 // function to be run when a submission is made with id #calculator
 // This will attempt to post a new row to cashflows
 const onAddCashflow = function (event) {
   event.preventDefault()
+
+  // const data = [$('.selected td:eq(1)').text()]
   const data = getFormFields(this)
   // ajax request
   api.addCashflow(data)
@@ -43,10 +49,10 @@ const onAddCashflow = function (event) {
 
 const onDeleteCashflow = function (event) {
   event.preventDefault()
-  // get the data-id of the selected row for deletion
-  const data = $('.selected').attr('data-id')
+  // get the data-id of the selected row
+  const id = $('.selected').attr('data-id')
   // ajax request
-  api.deleteCashflow(data)
+  api.deleteCashflow(id)
   // on success
   .then(ui.deleteCashflowSuccess)
   // make ajax request GET request for users index
@@ -59,9 +65,21 @@ const onDeleteCashflow = function (event) {
 
 const onUpdateCashflow = function (event) {
   event.preventDefault()
+  const target = $('.selected')
+  // get the data-id of the selected row
+  const id = target.attr('data-id')
+  // get cashflow name of selected row
+  // console.log('this is id, name, and value', id, name, value)
+  // const data = {
+  //   cashflow: {
+  //     id: id,
+  //     name: name,
+  //     value: value
+  //   }
+  // }
   const data = getFormFields(this)
   // ajax request
-  api.updateCashflow(data)
+  api.updateCashflow(id, data)
   // on success
   .then(ui.updateCashflowSuccess)
   .then(api.indexCashflow)
